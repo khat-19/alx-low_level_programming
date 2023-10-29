45efdaae1cdbb541dfed38cb05c7ca63b162f3cb
Mawuliah Mansaray <mawuliahlmansaray@gmail.com>
2023-10-29 15:29:49 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main -  a program that print its own name
+*@argc: argument count
+*@argv: argument vector
+*Return: 0
+*/
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
